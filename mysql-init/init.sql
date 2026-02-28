52a349d05aa5800a001c96d508cc753fa7a8ab1c
Cristian Camilo Cortés Hernández <camilo.cortes@pragma.com.co>
2026-02-28 12:03:52 -0500

    Se corrigió sql de inicialización

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,11 +1,5 @@
 CREATE DATABASE IF NOT EXISTS raw;
-CREATE DATABASE IF NOT EXISTS staging;
 CREATE DATABASE IF NOT EXISTS curated;
-
-GRANT ALL PRIVILEGES ON raw.* TO 'user'@'%';
-GRANT ALL PRIVILEGES ON staging.* TO 'user'@'%';
-GRANT ALL PRIVILEGES ON curated.* TO 'user'@'%';
-FLUSH PRIVILEGES;
 
 CREATE TABLE IF NOT EXISTS raw.raw_penguins (
             id INT,
@@ -17,3 +11,7 @@ CREATE TABLE IF NOT EXISTS raw.raw_penguins (
             body_mass_g INT,
             sex INT,
             year INT);
+
+GRANT ALL PRIVILEGES ON raw.* TO 'user'@'%';
+GRANT ALL PRIVILEGES ON curated.* TO 'user'@'%';
+FLUSH PRIVILEGES;
